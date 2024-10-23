d84b68000c2bb1b65c85695ef14200f93327fff6
YingshQi <qiyingsh@usc.edu>
2024-10-23 15:21:30 +0100

    Edits to Chapter 2 and Chapter 3 - Data selection and borough noise complaint analysis.

--- cookbook/Chapter 3 - Which borough has the most noise complaints (or, more selecting data).py
+++ cookbook/Chapter 3 - Which borough has the most noise complaints (or, more selecting data).py
@@ -1,6 +1,7 @@
 # %%
 import pandas as pd
 import matplotlib.pyplot as plt
+import polars as pl
 
 
 # Make the graphs a bit prettier, and bigger
@@ -15,7 +16,7 @@ pd.set_option("display.max_columns", 60)
 # %%
 # Let's continue with our NYC 311 service requests example.
 # because of mixed types we specify dtype to prevent any errors
-complaints = pd.read_csv("../data/311-service-requests.csv", dtype="unicode")
+pl_complaints = pl.read_csv("../data/311-service-requests.csv", infer_schema_length=0)
 
 # %%
 # TODO: rewrite the above using the polars library (you might have to import it above) and call the data frame pl_complaints
@@ -23,15 +24,15 @@ complaints = pd.read_csv("../data/311-service-requests.csv", dtype="unicode")
 # %%
 # 3.1 Selecting only noise complaints
 # I'd like to know which borough has the most noise complaints. First, we'll take a look at the data to see what it looks like:
-complaints[:5]
+pl_complaints.head(5)
 
 # %%
 # TODO: rewrite the above in polars
 
 # %%
 # To get the noise complaints, we need to find the rows where the "Complaint Type" column is "Noise - Street/Sidewalk".
-noise_complaints = complaints[complaints["Complaint Type"] == "Noise - Street/Sidewalk"]
-noise_complaints[:3]
+pl_noise_complaints = pl_complaints.filter(pl_complaints["Complaint Type"] == "Noise - Street/Sidewalk")
+pl_noise_complaints.head(3)
 
 # %%
 # TODO: rewrite the above in polars
@@ -39,9 +40,9 @@ noise_complaints[:3]
 
 # %%
 # Combining more than one condition
-is_noise = complaints["Complaint Type"] == "Noise - Street/Sidewalk"
-in_brooklyn = complaints["Borough"] == "BROOKLYN"
-complaints[is_noise & in_brooklyn][:5]
+is_noise = pl_complaints["Complaint Type"] == "Noise - Street/Sidewalk"
+in_brooklyn = pl_complaints["Borough"] == "BROOKLYN"
+pl_complaints.filter(is_noise & in_brooklyn).head(5)
 
 # %%
 # TODO: rewrite the above using the Polars library. In polars these conditions are called Expressions.
@@ -50,9 +51,9 @@ complaints[is_noise & in_brooklyn][:5]
 
 # %%
 # If we just wanted a few columns:
-complaints[is_noise & in_brooklyn][
+pl_complaints.filter(is_noise & in_brooklyn).select(
     ["Complaint Type", "Borough", "Created Date", "Descriptor"]
-][:10]
+).head(10)
 
 # %%
 # TODO: rewrite the above using the polars library
@@ -60,9 +61,9 @@ complaints[is_noise & in_brooklyn][
 
 # %%
 # 3.3 So, which borough has the most noise complaints?
-is_noise = complaints["Complaint Type"] == "Noise - Street/Sidewalk"
-noise_complaints = complaints[is_noise]
-noise_complaints["Borough"].value_counts()
+is_noise = pl_complaints["Complaint Type"] == "Noise - Street/Sidewalk"
+pl_noise_complaints = pl_complaints.filter(is_noise)
+pl_noise_complaints.groupby("Borough").agg(pl.count()).sort("count", reverse=True)
 
 # %%
 # TODO: rewrite the above using the polars library
@@ -70,10 +71,15 @@ noise_complaints["Borough"].value_counts()
 
 # %%
 # What if we wanted to divide by the total number of complaints?
-noise_complaint_counts = noise_complaints["Borough"].value_counts()
-complaint_counts = complaints["Borough"].value_counts()
+noise_complaint_counts = pl_noise_complaints.groupby("Borough").agg(pl.count().alias("noise_count"))
+complaint_counts = pl_complaints.groupby("Borough").agg(pl.count().alias("total_count"))
 
-noise_complaint_counts / complaint_counts.astype(float)
+normalized_complaints = noise_complaint_counts.join(complaint_counts, on="Borough")
+normalized_complaints = (normalized_complaints.with_columns(
+    (pl.col("noise_count") / pl.col("total_count")).alias("normalized_ratio")
+))
+
+normalized_complaints.select(["Borough", "normalized_ratio"])
 
 # %%
 # TODO: rewrite the above using the polars library
@@ -81,7 +87,8 @@ noise_complaint_counts / complaint_counts.astype(float)
 
 # %%
 # Plot the results
-(noise_complaint_counts / complaint_counts.astype(float)).plot(kind="bar")
+normalized_complaints_pd = normalized_complaints.select(["Borough", "normalized_ratio"]).to_pandas()
+normalized_complaints_pd.plot(kind="bar", x="Borough", y="normalized_ratio")
 plt.title("Noise Complaints by Borough (Normalized)")
 plt.xlabel("Borough")
 plt.ylabel("Ratio of Noise Complaints to Total Complaints")
